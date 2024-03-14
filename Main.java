c5edce6bcba4530bfcb821958b037bef194d6944
Sergio <sergio.hanganu@gmail.com>
2024-03-14 01:56:43 +0100

    Work done

---
+++ Main.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(new Student("Sergio",22));
+        students.add(new Student("Giuseppe",32));
+        students.add(new Student("Marco",23));
+        students.add(new Student("Lorenzo",24));
+        students.add(new Student("Andrea",25));
+        students.add(new Student("Francesco",16));
+        students.add(new Student("Franco",26));
+        students.add(new Student("Michele",35));
+        students.add(new Student("Alessandro",44));
+        students.add(new Student("GianMarco",12));
+        students.add(new Student("Sergiu",33));
+        students.add(new Student("Alessio",11));
+
+
+        Collections.sort(students, Comparator.comparingInt(Student::getAge));
+
+        System.out.println("\nArrayList ordinato per età:");
+        printStudents(students);
+    }
+    public static void printStudents(ArrayList<Student> students) {
+        for (Student student : students) {
+            System.out.println("Name: " + student.getName() + ", Age: " + student.getAge());
+        }
+    }
+}
+
+
+
